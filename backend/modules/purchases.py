6e9a3f47a47769a6a9b470033456fdfaa68f1177
King T Chingaona <kingtimothy495@gmail.com>
2026-07-03 13:42:52 +0200

    Fix: Correcting to avoid rerun errors

--- backend/modules/purchases.py
+++ backend/modules/purchases.py
@@ -399,7 +399,7 @@ def purchases_page():
                     st.error("Please enter an item name")
                 
                 st.session_state.button_clicked = False
-                st.rerun()
+                #st.rerun()
         
         # Display PO Cart
         if st.session_state.po_cart:
